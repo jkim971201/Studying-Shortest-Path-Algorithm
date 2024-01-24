5ebcd216cd473cb3df5f387562000557347e755e
jkim971201 <jkim97@postech.ac.kr>
2024-01-24 22:08:12 +0900

    0124

--- src/Grid.h
+++ src/Grid.h
@@ -1,35 +1,53 @@
 #pragma once
 
 #include <vector>
+#include <cmath>
 
 class Grid
 {
-	public:
+  public:
 
-		Grid(int width, int height, int xS, int yS, int xT, int yT)
-			: width_  (  width),
-			  height_ ( height),
-				xS_     (     xS),
-				yS_     (     yS),
-				xT_     (     xT),
-				yT_     (     yT)
-		{}
+    Grid(int width, int height, int xS, int yS, int xT, int yT)
+      : width_  (  width),
+        height_ ( height),
+        xS_     (     xS),
+        yS_     (     yS),
+        xT_     (     xT),
+        yT_     (     yT)
+    {}
 
-		int width()  const { return width_;  }
-		int height() const { return height_; }
+    int width()  const { return width_;  }
+    int height() const { return height_; }
 
-		int xS() const { return xS_; }
-		int yS() const { return yS_; }
-		int xT() const { return xT_; }
-		int yT() const { return yT_; }
+    int xS() const { return xS_; }
+    int yS() const { return yS_; }
+    int xT() const { return xT_; }
+    int yT() const { return yT_; }
 
-	private:
+		// Euclidean
+//    float getHeuristicCost(int curX, int curY) const
+//    {
+//      float diffX = static_cast<float>(curX - xT_);
+//      float diffY = static_cast<float>(curY - yT_);
+//
+//      return std::sqrt(diffX * diffX + diffY * diffY);
+//    }
 
-		int width_;
-		int height_;
+    float getHeuristicCost(int curX, int curY) const
+    {
+      float diffX = static_cast<float>(curX - xT_);
+      float diffY = static_cast<float>(curY - yT_);
 
-		int xS_;
-		int yS_;
-		int xT_;
-		int yT_;
+      return std::abs(diffX) + std::abs(diffY);
+		}
+
+  private:
+
+    int width_;
+    int height_;
+
+    int xS_;
+    int yS_;
+    int xT_;
+    int yT_;
 };
--- src/main.cpp
+++ src/main.cpp
@@ -1,28 +1,58 @@
 #include <iostream>
 #include <string>
 #include <stdio.h>
+#include <queue>
 
 #include "Painter.h"
 #include "Grid.h"
 
 using namespace gui;
 
+struct Node
+{
+  int x = 0;
+  int y = 0;
+	float costG = 0.0;
+  float costF = 0.0;
+
+  Node(int _x, int _y)
+    : x(_x), y(_y)
+  {}
+
+  // To make queue as a min priority
+  bool operator<(const Node other) const { return (costF > other.costF); };
+};
+
+class AStar
+{
+  public:
+
+    AStar(Grid* grid)
+      : grid_ (grid)
+    {}
+
+  private:
+
+    std::priority_queue<Node> queue_;
+    Grid* grid_;
+};
+
 int main(int argc, char* argv[]) 
 {
-	int width  = 32;
-	int height = 32;
+  int width  = 32;
+  int height = 32;
 
-	int startX = 0;
-	int startY = 0;
+  int startX = 0;
+  int startY = 0;
 
-	int goalX = width - 1;
-	int goalY = height - 1;
+  int goalX = width - 1;
+  int goalY = height - 1;
 
-	Grid grid(width, height, startX, startY, goalX, goalY);
+  Grid grid(width, height, startX, startY, goalX, goalY);
 
   QApplication app(argc, argv);
   Painter vis(&app, &grid);
   vis.openWindow();
 
-	return 0;
+  return 0;
 }
